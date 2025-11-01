1f427fab24decc645f6ecfe4c049ea9e6c18e4db
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-11-01 21:48:23 +0530

    Update updates.js

--- routes/updates.js
+++ routes/updates.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const User1 = require('../models/User1');
+const User1 = require('../models/user');
 const Blog = require('../models/blog');
 const multer = require('multer');
 const path = require('path');
